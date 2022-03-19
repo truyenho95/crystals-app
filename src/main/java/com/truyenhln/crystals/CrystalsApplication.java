e3bff03b6ea744de3411c274c58d78cc5d5539ea
truyenho95 <truyenho.webdev@gmail.com>
2022-03-19 16:25:08 +0700

    Init project

---
+++ src/main/java/com/truyenhln/crystals/CrystalsApplication.java
@@ -0,0 +1,13 @@
+package com.truyenhln.crystals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrystalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrystalsApplication.class, args);
+    }
+
+}
