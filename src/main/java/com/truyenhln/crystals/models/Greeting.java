3951cd06a57af2628ddf87d9e54e2f93f5025726
truyenho95 <truyenho.webdev@gmail.com>
2022-03-19 22:44:54 +0700

    - Implement spring-boot-starter-web - Create greeting api (dummy api) - Change tomcat starting portf from default (8080) to 16001

---
+++ src/main/java/com/truyenhln/crystals/models/Greeting.java
@@ -0,0 +1,15 @@
+package com.truyenhln.crystals.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+}
